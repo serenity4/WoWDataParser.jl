24a98416ff7ff77ff39b95883ed25c665b6829ea
serenity4 <cedric.bel@hotmail.fr>
2024-12-28 15:12:18 +0100

    Export LString, add conversion methods for LString

--- src/dbc/localization.jl
+++ src/dbc/localization.jl
@@ -90,3 +90,8 @@ end
 Base.show(io::IO, lstr::LString) = print(io, 'l', sprint(show, lstr[]))
 
 macro l_str(ex) :(LString($(esc(ex)))) end
+
+WoWBase.query_param(x::LString) = x[]
+
+Base.convert(::Type{String}, lstr::LString) = lstr[]
+Base.convert(::Type{LString}, str::AbstractString) = LString(str)
